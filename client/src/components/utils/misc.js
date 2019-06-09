ed884949b42a76053b82f9615900a6936a209409
nick <nickartes@gmail.com>
2019-06-09 11:19:52 +0000

    first commit

---
+++ client/src/components/utils/misc.js
@@ -0,0 +1,3 @@
+// SERVER ROUTES
+export const USER_SERVER = 'https://paindemo-nicknak.c9users.io:8080/api/users';
+export const PRODUCT_SERVER = 'https://paindemo-nicknak.c9users.io:8080/api/product';
